7115786d905fb1612a955c9643a3b6a50ebcd540
ismeniabrandao <brandaoismenia@gmail.com>
2023-05-09 23:19:36 +0200

    Initial Commit

---
+++ VIBES 1.0/VIBES_1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  VIBES_1_0App.swift
+//  VIBES 1.0
+//
+//  Created by Ismenia Brandao on 2023-05-09.
+//
+
+import SwiftUI
+
+@main
+struct VIBES_1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
