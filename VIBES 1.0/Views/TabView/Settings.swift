61554179c955922f67606064bef656fdfa3b12f2
ismeniabrandao <brandaoismenia@gmail.com>
2023-05-14 13:27:36 +0200

    Setting Page

--- VIBES 1.0/Views/TabView/Settings.swift
+++ VIBES 1.0/Views/TabView/Settings.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct Settings: View {
     var body: some View {
-        Text("SETTINGS GIRL!")
+        
+        NavigationView {
+            
+            List {
+                Section {
+                    Button (action: {
+                        self.showFeatures()
+                    }) {
+                        SettingsCell(settingsTitle: "Features", imgName: "sparkles", clr: .yellow)
+                    }
+                }
+                
+                Section {
+                    Button (action: {
+                        self.showFeatures()
+                    }) {
+                        SettingsCell(settingsTitle: "Write a Review", imgName: "pencil.and.outline", clr: .yellow)
+                    }
+                    
+                    Button (action: {
+                        self.showFeatures()
+                    }) {
+                        SettingsCell(settingsTitle: "Tell your Friends", imgName: "gift", clr: .yellow)
+                    }
+                }
+                
+                Section {
+                    Button (action: {
+                        self.showFeatures()
+                    }) {
+                        SettingsCell(settingsTitle: "Suggest a feature", imgName: "star.circle", clr: .yellow)
+                    }
+                    
+                    Button (action: {
+                        self.showFeatures()
+                    }) {
+                        SettingsCell(settingsTitle: "Report a bug", imgName: "exclamationmark.triangle", clr: .yellow)
+                    }
+                }
+
+            }
+            .navigationBarTitle("Settings")
+        }
+    }
+    
+    func showFeatures() {
+        print("Show user Features")
     }
 }
 
