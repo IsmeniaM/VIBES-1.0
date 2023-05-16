3c62e77ea427c75f538ebf4af025d620b5a1c781
ismeniabrandao <brandaoismenia@gmail.com>
2023-05-16 14:23:05 +0200

    FavoritePage

--- VIBES 1.0/Models/Concert.swift
+++ VIBES 1.0/Models/Concert.swift
@@ -15,32 +15,33 @@ struct Concert: Identifiable, Equatable {
     let time: String
     let address: String
     let price: Int
+    let isFavorite: Bool
     var quantity: Int = 1
     
     
 }
 
 let concertsList = [
-    Concert(name: "Scenkursföreställning", date: "20/05/2023", image: "event1", time: "19:00", address: "Birger Jarlsgatan 70", price: 350),
+    Concert(name: "Scenkursföreställning", date: "20/05/2023", image: "event1", time: "19:00", address: "Birger Jarlsgatan 70", price: 350, isFavorite: false),
     
-    Concert(name: "Svartklubben - Mat & Musik i Mörker", date: "16/05/2023", image: "event2", time: "19:00", address: "Birger Jarlsgatan 70", price: 350),
+    Concert(name: "Svartklubben - Mat & Musik i Mörker", date: "16/05/2023", image: "event2", time: "19:00", address: "Birger Jarlsgatan 70", price: 350, isFavorite: false),
     
-    Concert(name: "Giveon", date: "16/05/2023", image: "event3", time: "18:00", address: "Cirkus Arena och Restaurang på Djurgården", price: 350),
+    Concert(name: "Giveon", date: "16/05/2023", image: "event3", time: "18:00", address: "Cirkus Arena och Restaurang på Djurgården", price: 350, isFavorite: false),
     
-    Concert(name: "NUET - Mind Against & Marino Canal", date: "17/05/2023", image: "event4", time: "18:00", address: "Cirkus", price: 350),
+    Concert(name: "NUET - Mind Against & Marino Canal", date: "17/05/2023", image: "event4", time: "18:00", address: "Cirkus", price: 350, isFavorite: false),
     
-    Concert(name: "Steve Hackett", date: "18/05/2023", image: "event5", time: "18:00", address: "Cirkus", price: 350),
+    Concert(name: "Steve Hackett", date: "18/05/2023", image: "event5", time: "18:00", address: "Cirkus", price: 350, isFavorite: false),
     
-    Concert(name: "Svartklubben - Mat & Musik i Mörker", date: "18/05/2023", image: "event2", time: "18:00", address: "Birger Jarlsgatan 70", price: 350),
+    Concert(name: "Svartklubben - Mat & Musik i Mörker", date: "18/05/2023", image: "event2", time: "18:00", address: "Birger Jarlsgatan 70", price: 350, isFavorite: false),
     
-    Concert(name: "HAIR - Musikalen", date: "18/05/2023", image: "event6", time: "18:00", address: "Cirkus", price: 350),
+    Concert(name: "HAIR - Musikalen", date: "18/05/2023", image: "event6", time: "18:00", address: "Cirkus", price: 350, isFavorite: false),
     
-    Concert(name: "CLUB AFTER DARK", date: "19/05/2023", image: "event7", time: "18:00", address: "Vasateatern", price: 350),
+    Concert(name: "CLUB AFTER DARK", date: "19/05/2023", image: "event7", time: "18:00", address: "Vasateatern", price: 350, isFavorite: false),
     
-    Concert(name: "MAFFIA COMEDY SUPERWEEKEND", date: "19/05/2023", image: "event8", time: "18:00", address: "Scandic Malmen", price: 350),
+    Concert(name: "MAFFIA COMEDY SUPERWEEKEND", date: "19/05/2023", image: "event8", time: "18:00", address: "Scandic Malmen", price: 350, isFavorite: false),
     
-    Concert(name: "Fredrik ”Benke” Rydman – Master Of Dance", date: "19/05/2023", image: "event9", time: "18:00", address: "Elverket, Linnégatan 69", price: 350),
+    Concert(name: "Fredrik ”Benke” Rydman – Master Of Dance", date: "19/05/2023", image: "event9", time: "18:00", address: "Elverket, Linnégatan 69", price: 350, isFavorite: false),
     
-    Concert(name: "Andreas Lundstedt – Freedom", date: "19/05/2023", image: "event10", time: "18:00", address: "Cirkus", price: 350)
+    Concert(name: "Andreas Lundstedt – Freedom", date: "19/05/2023", image: "event10", time: "18:00", address: "Cirkus", price: 350, isFavorite: false)
     
 ]
