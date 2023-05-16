240b82df436daa1c53e39db2371176a9abb2b097
Menia <78113804+IsmeniaM@users.noreply.github.com>
2023-05-17 01:56:53 +0200

    (fix)

--- VIBES 1.0/Views/ContentView.swift
+++ VIBES 1.0/Views/ContentView.swift
@@ -2,22 +2,41 @@
 //  ContentView.swift
 //  VIBES 1.0
 //
-//  Created by Ismenia Brandao on 2023-05-09.
+//  Created by Ismenia Brandao on 2023-05-17.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelection = 1
+    @StateObject var cartManager = CartManager()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        Text("dev")
-            .padding()
+        
+        TabView(selection: $tabSelection) {
+            Home()
+                .environmentObject(cartManager)
+                .tag(1)
+            Search()
+                .environmentObject(cartManager)
+                .tag(2)
+            Favorites()
+                .environmentObject(cartManager)
+                .tag(3)
+            Profile()
+                .tag(4)
+            Settings()
+                .tag(5)
+        }
+        .overlay(alignment: .bottom) {
+            CostumTabView(tabSelection: $tabSelection)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
